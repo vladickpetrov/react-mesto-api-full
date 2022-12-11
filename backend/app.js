14393db0d4a9e442e3894933d25ea5120a743e64
vladickpetrov <minnesota777@mail.ru>
2022-12-11 22:05:43 +0300

    crash test

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,12 @@ app.use(helmet());
 app.use(limiter);
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
